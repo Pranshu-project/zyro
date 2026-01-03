c23a6db05cf42034abe4531ba138ef90eb1052c1
kanha_workspace <kanhaiya.gupta@ez.works>
2026-01-04 03:57:45 +0530

    updated main.py

--- backend/main.py
+++ backend/main.py
@@ -40,7 +40,8 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5174",
-        "https://zyro-g2c4.onrender.com"
+        "https://zyro-g2c4.onrender.com",
+        "https://zyro-67cv9686v-pranshus-projects-daa60a75.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
